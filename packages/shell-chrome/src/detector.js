7ac29e9231ce97382413ddab1521fc74bffa83e0
spamvictus <49518627+spamvictus@users.noreply.github.com>
2023-10-04 15:18:38 +0200

    ditch the shit

--- packages/shell-chrome/src/detector.js
+++ packages/shell-chrome/src/detector.js
@@ -1,99 +1,107 @@
-import { installToast } from '@back/toast'
-import { isFirefox } from '@vue-devtools/shared-utils'
+import { installToast } from "@back/toast";
+import { isFirefox } from "@vue-devtools/shared-utils";
 
-window.addEventListener('message', e => {
+window.addEventListener("message", (e) => {
   if (e.source === window && e.data.vueDetected) {
-    chrome.runtime.sendMessage(e.data)
+    chrome.runtime.sendMessage(e.data);
   }
-})
+});
 
-function detect (win) {
-  let delay = 1000
-  let detectRemainingTries = 10
+function detect(win) {
+  let delay = 1000;
+  let detectRemainingTries = 10;
 
-  function runDetect () {
+  function runDetect() {
     // Method 1: Check Nuxt
-    const nuxtDetected = !!(window.__NUXT__ || window.$nuxt)
+    const nuxtDetected = !!(window.__NUXT__ || window.$nuxt);
 
     if (nuxtDetected) {
-      let Vue
+      let Vue;
 
       if (window.$nuxt) {
-        Vue = window.$nuxt.$root && window.$nuxt.$root.constructor
+        Vue = window.$nuxt.$root && window.$nuxt.$root.constructor;
       }
 
-      win.postMessage({
-        devtoolsEnabled: (/* Vue 2 */ Vue && Vue.config.devtools) ||
-          (/* Vue 3.2.14+ */ window.__VUE_DEVTOOLS_GLOBAL_HOOK__ && window.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled),
-        vueDetected: true,
-        nuxtDetected: true,
-      }, '*')
+      win.postMessage(
+        {
+          devtoolsEnabled: true,
+          vueDetected: true,
+          nuxtDetected: true,
+        },
+        "*"
+      );
 
-      return
+      return;
     }
 
     // Method 2: Check  Vue 3
-    const vueDetected = !!(window.__VUE__)
+    const vueDetected = !!window.__VUE__;
     if (vueDetected) {
-      win.postMessage({
-        devtoolsEnabled: /* Vue 3.2.14+ */ window.__VUE_DEVTOOLS_GLOBAL_HOOK__ && window.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled,
-        vueDetected: true,
-      }, '*')
+      win.postMessage(
+        {
+          devtoolsEnabled: true,
+          vueDetected: true,
+        },
+        "*"
+      );
 
-      return
+      return;
     }
 
     // Method 3: Scan all elements inside document
-    const all = document.querySelectorAll('*')
-    let el
+    const all = document.querySelectorAll("*");
+    let el;
     for (let i = 0; i < all.length; i++) {
       if (all[i].__vue__) {
-        el = all[i]
-        break
+        el = all[i];
+        break;
       }
     }
     if (el) {
-      let Vue = Object.getPrototypeOf(el.__vue__).constructor
+      let Vue = Object.getPrototypeOf(el.__vue__).constructor;
       while (Vue.super) {
-        Vue = Vue.super
+        Vue = Vue.super;
       }
-      win.postMessage({
-        devtoolsEnabled: Vue.config.devtools,
-        vueDetected: true,
-      }, '*')
-      return
+      win.postMessage(
+        {
+          devtoolsEnabled: true,
+          vueDetected: true,
+        },
+        "*"
+      );
+      return;
     }
 
     if (detectRemainingTries > 0) {
-      detectRemainingTries--
+      detectRemainingTries--;
       setTimeout(() => {
-        runDetect()
-      }, delay)
-      delay *= 5
+        runDetect();
+      }, delay);
+      delay *= 5;
     }
   }
 
   setTimeout(() => {
-    runDetect()
-  }, 100)
+    runDetect();
+  }, 100);
 }
 
 // inject the hook
 if (document instanceof HTMLDocument) {
-  installScript(detect)
-  installScript(installToast)
+  installScript(detect);
+  installScript(installToast);
 }
 
-function installScript (fn) {
-  const source = ';(' + fn.toString() + ')(window)'
+function installScript(fn) {
+  const source = ";(" + fn.toString() + ")(window)";
 
   if (isFirefox) {
     // eslint-disable-next-line no-eval
-    window.eval(source) // in Firefox, this evaluates on the content window
+    window.eval(source); // in Firefox, this evaluates on the content window
   } else {
-    const script = document.createElement('script')
-    script.textContent = source
-    document.documentElement.appendChild(script)
-    script.parentNode.removeChild(script)
+    const script = document.createElement("script");
+    script.textContent = source;
+    document.documentElement.appendChild(script);
+    script.parentNode.removeChild(script);
   }
 }
